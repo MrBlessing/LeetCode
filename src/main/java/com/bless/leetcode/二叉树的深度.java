f65f7cc050cf45fe7e6aa864f79609af4c62492b
bless <2366365981@qq.com>
2022-09-18 16:52:18 +0800

    二叉树的深度

---
+++ src/main/java/com/bless/leetcode/二叉树的深度.java
@@ -0,0 +1,40 @@
+package com.bless.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 二叉树的深度 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // 广度遍历
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
