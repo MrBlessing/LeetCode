b01fd702529ab7c27ee65c3827a0f0d274bd6017
MrLi <2366365981@qq.com>
2022-09-30 10:46:35 +0800

    不用加减乘除做加法

---
+++ src/main/java/com/bless/leetcode/不用加减乘除做加法.java
@@ -0,0 +1,16 @@
+package com.bless.leetcode;
+
+public class 不用加减乘除做加法 {
+    public int add(int a, int b) {
+        // 进位 = a & b << 1;
+        // 不进位相加的结果 = a 异或 b
+        // a + b = 进位 + 不进位相加的结果
+        // 不断的循环，直到carry = 0
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
